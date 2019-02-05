ba3bc0ca0b52ec4a901e6002c24c31c466ca2c2d
august <august@moos-bjerre.dk>
2019-02-05 13:23:04 +0100

    Function now returns infaltion and unemployment, basen on the other 3 parameters, plotting has begun

--- Philips curve.py
+++ Philips curve.py
@@ -9,23 +9,29 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 
-def philipC(UnEm,NatUnEm,Inflation,Ex_inflation,alpha=False):
+#edit it need to take only natural unemployment, expected inflation and alpha as input, and output Inflation and unemployment
+def philipC(NatUnEm,Ex_inflation,alpha=False):
     if alpha == False:
         sys.exit("Error: You have to provide an alpha value")
-    UnEm1 = NatUnEm - alpha*(Inflation-Ex_inflation) 
-    NatUnEm1 = UnEm + alpha*(Inflation-Ex_inflation)
-    Inflation1 = Ex_inflation + (UnEm-NatUnEm)/alpha
-    Ex_inflation1 = (UnEm-NatUnEm)/alpha - Inflation
+    #UnEm1 = NatUnEm - alpha*(Ex_inflation + ((UnEm-NatUnEm)/alpha)-Ex_inflation) 
+    #My rather bad math tells me that unemployment must be given by 
+    UnEm = NatUnEm - ((-alpha*Ex_inflation)-Ex_inflation)/2
+   # NatUnEm1 = UnEm + alpha*(Inflation-Ex_inflation)
+    Inflation = Ex_inflation + (NatUnEm -(((-alpha*Ex_inflation)-Ex_inflation)/2)-NatUnEm)/alpha
+    #Ex_inflation1 = (UnEm-NatUnEm)/alpha - Inflation
    
-    all_vars = list([UnEm1,NatUnEm1,Inflation1,Ex_inflation1,alpha])
-    return(Inflation1,UnEm1,NatUnEm1,Ex_inflation1)
+    all_vars = list([UnEm, Inflation])
+    return(Inflation,UnEm)
 
-x = philipC(UnEm=2,NatUnEm=2,Inflation=0.2,Ex_inflation=0.1,alpha=0.3)
+x = philipC(NatUnEm=2,Ex_inflation=4,alpha=0.3)
 inflation = x[0]
 Unemployment = x[1]
+Natural_Unemployment = x[2]
+Expectet_Inflation = x[3]
 
 x = list([0,inflation])
 y = list([Unemployment,0])
 
 plt.scatter(y,x)
 plt.show()
+print(inflation,Unemployment)
