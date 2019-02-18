8c621c948dd2293e323774dad7db40308c56c029
august <august@moos-bjerre.dk>
2019-02-18 15:43:43 +0100

    "Script that makes an interactive plot of the philips curve (in the short run)"

--- new PC.py
+++ new PC.py
@@ -5,26 +5,31 @@ import matplotlib.widgets as wig
 from ipywidgets import interact, interactive, fixed, interact_manual
 import ipywidgets as widgets
 
-def philipC(NatUnEm,Ex_inflation,alpha=0):
-    if alpha == False:
-        sys.exit("Error: You have to provide an alpha value")
+def philipC(NatUnEm,Ex_inflation,alpha):
+    """This function takes values of Natural unemployment, expectec inflation, and the alpha value for the trade-off between those.
+        The function then defines the Unemployment rate at inflation level 0.2 and vice verca. This is used to plot the a straigt line
+        The deriviations from Makiew and Taylor's macro economics. The function returns unemployment and inflation"""
     Inflation = 0.2
     UnEm = NatUnEm + (Ex_inflation - Inflation)/alpha
     Unemployment = 0.2
     Inflation = Ex_inflation - alpha*(Unemployment - NatUnEm)
 
 
-    return list([UnEm,Inflation,Ex_inflation])
+    return list([UnEm,Inflation])
 
 
-def PCplot(NatUnEm,Ex_inflation,alpha=0):
+def PCplot(NatUnEm,Ex_inflation,alpha=1):
+      """This function plots the first two values given to it, as Unemployment and Inflation. It plots it by assuming that the other value is 0.2 (as calculated in philipsC function)""""
     x = philipC(NatUnEm,Ex_inflation,alpha) 
     plt.plot([0.2,x[0]],[x[1],0.2])
     plt.xlabel("Unemployment")
     plt.ylabel("Inflation")
-    plt.xlim(0,5)
-    plt.ylim(0,8)
+    plt.xlim(0,10)
+    plt.ylim(0,10)
 
 %matplotlib inline
-exi_slider = widgets.IntSlider(min=0,max=10,value=2,step=0.5)
-interact(PCplot, Ex_inflation =  widgets.FloatSlider(min=0,max=5,value=2,step=0.2) ,NatUnEm = 2, alpha = 2)
+style = {'description_width': 'initial'}
+exi_slider = widgets.FloatSlider(min=0,max=10,value=2,step=0.5,description = "Expected inflation",style=style)
+alp_slider = widgets.FloatSlider(min=0.1,max=3,value=1,step=0.2,description = "Alpha value for trade-off",style=style)
+NUE_slider = widgets.FloatSlider(min=0,max=7,value=2,step=0.5, description ="Natural unemployment",style=style)
+interact(PCplot, Ex_inflation =  exi_slider ,NatUnEm = NUE_slider, alpha =  alp_slider)
